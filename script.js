0f2c4389e34c788fc28d9e8310178387357e674a
Ziggiotti Andrea <ziggiottiandrea1@gmail.com>
2020-08-14 19:18:12 +0200

    add mobile version

--- script.js
+++ script.js
@@ -1,15 +1,29 @@
  function op(){
  	
  	var cer = document.getElementById("cerchio");
- 	var opa = 0;
- 	var id = setInterval(frame, 5);
+ 	var opa = 1;
+ 	var top=true;
+ 	var id = setInterval(frame, 10);
+ 	var n = 0;
+ 	
   function frame() {
-    if (opa == 1) {
+    if (opa == 2) {
       clearInterval(id);
     } else {
-      opa += 0.01; 
+    			  
+  		if(!top){
+  		
+  		if(opa == 1){ top=true }
+      opa += 0.005; 
       cer.style.opacity = opa; 
     }
+    else{
+    
+    	if(opa==0){top=false}
+    	opa -= 0.005;
+    	cer.style.opacity = opa;
+    	}
+    	n++;
+    }
   }
- 	
- }
+  }
